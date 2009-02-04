d10e9e3dfbd17af830a89e522125e433ebc7d26b
kent <kent@machina.no>
2009-02-04 12:07:13 +0100

    fixed findPageByPath test and r3emoved unused json test

--- test/integration/ZootPageTests.groovy
+++ test/integration/ZootPageTests.groovy
@@ -4,19 +4,19 @@ class ZootPageTests extends GroovyTestCase {
 
 
 	void testFindPageByPath(){
-		def p1 = new ZootPage(title:"p1", author:"meg")
+		def p1 = new ZootPage(title:"p1", author:"meg",pos:1)
     if( !p1.save() ) {
       p1.errors.each {
         println it
       }
     }
-    def p2 = new ZootPage(title:"p2", slug:"2", author:"meg", parent:p1)
+    def p2 = new ZootPage(title:"p2", slug:"2", author:"meg", parent:p1,pos:2)
     if( !p2.save() ) {
       p2.errors.each {
         println it
       }
     }
-		def p3 = new ZootPage(title:"p3", slug:"3", author:"meg", parent: p2)
+		def p3 = new ZootPage(title:"p3", slug:"3", author:"meg", parent: p2,pos: 3)
     if( !p3.save() ) {
       p3.errors.each {
         println it
@@ -31,18 +31,6 @@ class ZootPageTests extends GroovyTestCase {
 		assertNull ZootPage.findPageByPath("1/blslblsd")
 	}
 
-	void testJSON(){
-		createSimplePageStructure()
-		def p2 = ZootPage.findPageByPath("2")
-		println p2.parent
-		println p2.children
-
-		def jsonoutput = p2.encodeAsXML()
-		println jsonoutput
-		def p = jsonoutput.decodeJSON()
-
-		assertEquals(p2,p)
-	}
 
 	private void createSimplePageStructure() {
 		def p1 = new ZootPage(title:"p1", author:"meg")
